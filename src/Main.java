927eaaf18477cde75bd6a63f7607eba9169b8af8
First project <ekulakov056@gmail.com>
2023-07-23 01:31:44 +0500

    Сделал всё через 1 переменную

--- src/Main.java
+++ src/Main.java
@@ -48,23 +48,18 @@ public class Main {
         if (age <= 2){
             System.out.println("Если возраст человека равен " + age + ", то ему пора спать");
         }
-        byte age = 5;
         if (age >= 2 && age <= 6){
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
         }
-        byte age = 1;
         if (age >= 7 && age < 18){
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
         }
-        byte age = 19;
         if (age >= 18 && age < 24){
             System.out.println("Если возраст человека равен " + age + ", то его место в университете");
         }
-        byte age = 25;
-        if (age >= 24 && age <= 60){
+       if (age >= 24 && age <= 60){
             System.out.println("Если возраст человека равен " + age + ", то ему пора ходить на работу");
         }
-        byte age = 61;
         if (age > 60){
             System.out.println("Если возраст человека равен " + age + ", то он может отдохнуть");
         }
@@ -72,17 +67,15 @@ public class Main {
     }
     public static void task5 () {
         System.out.println("Задача 5");
-        byte lessThan5Years = 4;
-        if (lessThan5Years < 5) {
-            System.out.println("Если возраст ребенка равен " + lessThan5Years + ", то он не может кататься на аттракционе");
+        byte age = 4;
+        if (age < 5) {
+            System.out.println("Если возраст ребенка равен " + age + ", то он не может кататься на аттракционе");
         }
-        byte from5To14Years = 7;
-        if (from5To14Years >= 5 && from5To14Years <=13){
-            System.out.println("Если возраст ребенка равен " + from5To14Years + ", то он может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзяю");
+        if (age >= 5 && age <=13){
+            System.out.println("Если возраст ребенка равен " + age + ", то он может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзяю");
         }
-        byte over14Years = 15;
-        if (over14Years >= 14){
-            System.out.println("Если возраст ребенка равен " + over14Years + ", то он может кататься без сопровождения взрослого.");
+        if (age >= 14){
+            System.out.println("Если возраст ребенка равен " + age + ", то он может кататься без сопровождения взрослого.");
         }
         System.out.println();
     }
